fd61abeb400ac5464a521dd87afd63ca45869b49
ChanhoKim <ch.kim@rsad.co.kr>
2020-08-28 18:23:52 +0900

    채팅창 레이아웃 만들기.

---
+++ Sample/js/many_layout.js
@@ -0,0 +1,26 @@
+$('.chat_slide').slick({    
+    slidesToShow: 5,
+    vertical: true,
+    autoplay:true,
+    autoplaySpeed:1000,    
+    draggable:false,
+    pauseOnHover : false
+});
+$(".slick-arrow").hide();
+
+var count = 0;
+var price = [4523,16000,5422,7896]
+function random_price(){
+    var target = document.querySelector('.change_money');
+    var write = price[count]
+    target.innerText = write
+    count ++;
+    if(count > 3){
+        count = 0
+    }
+}
+setInterval(random_price,1000);
+// Splitting({
+//     target : target,
+//     by : 'words'
+// });

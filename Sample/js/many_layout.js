b77a0be4dbe5a7c29920a831d2fa9ed22bebf38f
ChanhoKim <ch.kim@rsad.co.kr>
2020-09-04 18:37:06 +0900

    test

--- Sample/js/many_layout.js
+++ Sample/js/many_layout.js
@@ -8,19 +8,18 @@ $('.chat_slide').slick({
 });
 $(".slick-arrow").hide();
 
-var count = 0;
-var price = [4523,16000,5422,7896]
-function random_price(){
-    var target = document.querySelector('.change_money');
-    var write = price[count]
-    target.innerText = write
-    count ++;
-    if(count > 3){
-        count = 0
-    }
-}
-setInterval(random_price,1000);
-// Splitting({
-//     target : target,
-//     by : 'words'
-// });
+// var count = 0;
+// var price = [4523,16000,5422,7896]
+// function random_price(){
+//     var target = document.querySelector('.change_money');
+//     var write = price[count]
+//     target.innerText = write
+//     count ++;
+//     if(count > 3){
+//         count = 0
+//     }
+// }
+// setInterval(random_price,1000);
+
+var text = document.querySelector('.change_money');
+

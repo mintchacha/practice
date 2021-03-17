31c3ce82bab54b8b4023f832f400d7bc0db1ce0f
ChanhoKim <ch.kim@rsad.co.kr>
2021-03-17 11:43:22 +0900

    자바스크립트

--- javascript/js/prototype & ex6 class.js
+++ javascript/js/prototype & ex6 class.js
@@ -130,10 +130,21 @@ class Food {
         this.brands = [];
     }
     addBrand(brand){
-        this.brands.push();
+        this.brands.push(brand);
     }
     print(){
         console.log(`${this.name} 을 파는 음식점들:`);
         console.log(this.brands.join(','));
     }
 }
+
+const pizza = new Food('피자');
+pizza.addBrand('피자헛')
+pizza.addBrand('도미노 피자');
+
+const chicken = new Food('치킨');
+chicken.addBrand('굽네치킨');
+chicken.addBrand('BBQ');
+
+pizza.print();
+chicken.print();

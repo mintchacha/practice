8672493f7410dc945bf8c0ae6586b99478491dd1
ChanhoKim <ch.kim@rsad.co.kr>
2022-01-13 18:20:38 +0900

    220113js 공부

--- react-tutorial/TEST/main.js
+++ react-tutorial/TEST/main.js
@@ -1,8 +1,29 @@
-function User(first, last){
-    this.firstName = first;
-    this.lastNmae = last   
+class Vehicle{
+    constructor(name, wheel){
+        this.name = name
+        this.wheel = wheel
+    }
 }
+const myVehicle = new Vehicle('운송수단',2)
+console.log(myVehicle);
 
-const chanho = new User('Chanho', 'Kim');
+class Bicycle extends Vehicle{
+    constructor(name, wheel){
+        super(name,wheel)
+    }
+}
+const myBicycle = new Bicycle('삼천리', 2);
+const daughtersBicycle = new Bicycle('세발',3);
+console.log(myBicycle);
+console.log(daughtersBicycle);
 
-console.log(chanho);
+class Car extends Vehicle{
+    constructor(name, wheel, license){
+        super(name,wheel)
+        this.license = license
+    }
+}
+const myCar = new Car('벤츠', 4, true);
+const daughtersCar = new Car('포르쉐',4,false);
+console.log(myCar)
+console.log(daughtersCar);

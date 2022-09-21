e05021724cda950566da50b48330be41dffe4a72
ChanhoKim <ch.kim@rsad.co.kr>
2022-09-21 18:16:43 +0900

    상점탭추가, 아이템드랍기능 추가, 설정 로그삭제기능추가등

--- cha'sRPG/js/interact.js
+++ cha'sRPG/js/interact.js
@@ -1,13 +1,59 @@
-const user = new User('유저', 1, {max : 10, count : 10}, {max : 10, count : 10}, 1, 0, 0, [])
+// 유저정보 출력
+const user = new User(
+  // 이름
+  '유저',
+  // 레벨
+  1,
+  // 체력
+  {max : 10, count : 10},
+  // 마나
+  {max : 10, count : 10},
+  // 공격력
+  1,
+  // 방어력
+  0,
+  // 경험치
+  0,
+  // 골드  
+  0,
+  // 아이템
+  []
+)
 user.quiry()
 
+// 배틀 버튼
 const battle_btn = document.getElementById('battle')
 battle_btn.onclick = () => {
     battle(slime)        
 }
+const logdelete_btn = document.getElementById('logdelete')
+logdelete_btn.onclick = () => {
+  battlezone.querySelectorAll('p').forEach((item)=>{
+    item.remove()
+  })
+}
 
+// 가방 열기버튼
 const bag_btn = document.getElementById('bagopen')
-const bag = document.querySelector('.bagbox')
 bag_btn.onclick = () => {
   bag.classList.toggle('open')
+}
+
+// 설정 열기버튼
+const setting_btn = document.getElementById('settingopen')
+setting_btn.onclick = () => {
+  setting.classList.toggle('open')
+}
+
+// 상점이롱 버튼
+const goshop = document.getElementById('goshop')
+goshop.onclick = () => {
+  scrollTo(
+    {
+      top : shop.offsetTop,
+      left:0,
+      behavior : 'smooth'
+    }    
+  )
+  console.log(shop.scrollHeight)
 }
--- cha'sRPG/js/setting.js
+++ cha'sRPG/js/setting.js
@@ -1,5 +1,5 @@
 class Character{
-    constructor(name, lv, hp, mp, atk, def, exp){
+    constructor(name, lv, hp, mp, atk, def, exp,  gold, item){
         this.name = name
         this.lv = lv
         this.hp = hp
@@ -7,21 +7,23 @@ class Character{
         this.atk = atk
         this.def = def
         this.exp = exp
+        this.gold = gold
+        this.item = item
     }
 }
 // 로그생성
 function createlog(txt, color){
-    const p = document.createElement('p')
-    p.innerHTML = txt
-    if(color){
-        p.style.color = color
-    }else{
-        p.style.color = '#333'
-    }            
-    document.querySelector('main').appendChild(p)
+    const creattxt = document.createElement('p')
+    creattxt.innerHTML = txt
+    if(!color){color = '#Fff'}   
+    creattxt.style.color = color         
+    battlezone.appendChild(creattxt)
+    battlezone.scrollTop = battlezone.scrollHeight;
 }
 // 유저 생성
+// 레벨 업 포인트
 const level = [5,15,30,60,90,180]
+// 스탯창
 const namebox = document.getElementById('name')
 const lvbox = document.getElementById('lv')
 const maxhpbox = document.getElementById('maxhp')
@@ -31,11 +33,26 @@ const mpbox = document.getElementById('mp')
 const atkbox = document.getElementById('atk')
 const defbox = document.getElementById('def')
 const expbox = document.getElementById('exp')
+const goldbox = document.getElementById('gold')
+// 사냥터
+const battlezone = document.querySelector('.battlezone')
+// 설정
+const setting = document.querySelector('.settingbox')
+// 상점
+const shop = document.querySelector('.shop')
+// 인벤토리
+const bag = document.querySelector('.bagbox')
+for(let i = 1; i <= 35; i++){
+    const creatitem = document.createElement('div')        
+    bag.querySelector('.items').appendChild(creatitem)
+}
+const bag_items = document.querySelectorAll('.bagbox .items div')
+
 class User extends Character{
-    constructor(name, lv, hp, mp, atk, def, exp, inventory){
-        super(name, lv, hp, mp, atk, def, exp)
-        this.inventory = inventory
+    constructor(name, lv, hp, mp, atk, def, exp, gold, item){
+        super(name, lv, hp, mp, atk, def, exp, gold, item)
     }
+    // 스탯 DOM 출력
     quiry(){
         namebox.innerHTML = this.name
         lvbox.innerHTML = this.lv
@@ -46,7 +63,9 @@ class User extends Character{
         atkbox.innerHTML = this.atk
         defbox.innerHTML = this.def
         expbox.innerHTML = this.exp
+        goldbox.innerHTML = this.gold
     }
+    // 공격받는 이벤트
     damaged(moster){               
         this.hp.count -= moster.atk
         user.quiry()
@@ -56,65 +75,119 @@ class User extends Character{
             location.href = './';
         }
     }
-    getExp(get){
-        this.exp += get.exp 
+    // 경험치획득
+    getExp(getexp){
+        this.exp += getexp 
         level.forEach((item, index)=>{
             if(this.exp >= level[index]){
                 level.shift()
                 this.lv ++   
                 this.hp.count = this.hp.max 
                 createlog(`레벨업`, '#33dddf')
-                user.quiry()
             }
+            user.quiry()
         })
-    }            
+    }    
+    // 아이템획득
+    getitem(getitem){
+        this.item.push(getitem)
+        bag_items[this.item.length - 1].style.background = `url(${getitem.img}) no-repeat center center /80% rgb(255, 241, 214)`
+        bag_items[this.item.length - 1].style.cursor = 'pointer'
+        bag_items[this.item.length - 1].addEventListener('mouseover', function(){
+            this.classList.add('hover')
+            const createinfobox = document.createElement('div')
+            createinfobox.innerHTML = `<h4>${getitem.name}</h4>
+                                       <p>${getitem.info}</p>`
+            createinfobox.classList.add('iteminfo')
+            this.appendChild(createinfobox)
+        })
+        bag_items[this.item.length - 1].addEventListener('mouseout', function(){
+            this.classList.remove('hover')
+            this.querySelector('div.iteminfo').remove()
+        })
+    }
 }
 // 아이템 생성
 class Item {
-    constructor(type, name){
+    constructor(type, name, gold, img, info){
         this.type = type
         this.name = name
+        this.gold = gold
+        this.img = img
+        this.info = info
     }
 }
+// 무기생성
 class Weapon extends Item{
-    constructor(type, name, atk){
-        super(type, name)
+    constructor(type, name, gold, img, info, atk){
+        super(type, name, gold, img, info)
         this.atk = atk
     }
 }
+// 방어구생성
 class Armor extends Item{
-    constructor(type, name, def){
-        super(type, name)
+    constructor(type, name, gold, img, info, def){
+        super(type, name, gold, img, info)
         this.def = def
     }
 }
+// 회복아이템 생성
+class consumption extends Item{
+    constructor(type, name, gold, img, info, cure){
+        super(type, name, gold, img, info)
+        this.cure = cure
+    }
+}
 // 아이템 목록
-const Itemlist = [
-    // equip
-    new Weapon('weapon', 'Sword', 3),
-    new Armor('armor', 'plate', 1),
-    // etc
-    new Item('etc', 'slimebubble'),
-]        
+// 무기
+const woodsword = new Weapon('weapon', '목검', 5, './images/items/woodsword.png', '', 2)
+// 방어구
+const plate = new Armor('armor', '가죽 갑옷', 5, './images/items/plate.png', '', 1)
+// 소비
+const pill = new Item('consumption', '알약', 5, './images/items/Exitem.png', '체력을 5 회복시켜주는 약이다.', 5)
+// 기타
+const slimebubble = new Item('etc', '슬라임 방울', 0, './images/items/slimebubble.png', '',)
+
 // 몬스터 생성
 class Monster extends Character{
-    constructor(name, lv, hp, mp, atk, def, exp, item){
-        super(name, lv, hp, mp, atk, def, exp)
-        this.item = item
+    constructor(name, lv, hp, mp, atk, def, exp, gold, item){
+        super(name, lv, hp, mp, atk, def, exp, gold, item)        
     }
     damaged(user){                          
         this.hp.count -= user.atk                
+        console.log(user)
         createlog(`${this.name}은(는) ${user.atk}의 피해를 입었다. 남은 체력 ${this.hp.count}`)
         if(this.hp.count <= 0){
             createlog(`${this.name}을 해치웠다!`)
+            user.gold += this.gold
+            user.getitem(this.item)
+            user.getExp(this.exp)
             this.hp.count = this.hp.max
-            user.getExp(this)
-            inbattle = false
         }
     }
 }
 // 몬스터 목록
-const slime = new Monster('슬라임', 1, {max : 3, count : 3}, {max : 0, count : 0}, 1, 0, 5, 'slimebubble')
+const slime = new Monster(
+    // 이름
+    '슬라임',
+    // 레벨
+    1,
+    // 체력
+    {max : 3, count : 3},
+    // 마나
+    {max : 0, count : 0},
+    // 공격력
+    1,
+    // 방어력
+    0,
+    // 경험치
+    5,
+    // 골드
+    1,
+    // 드랍템
+    pill
+)
+const wolf = new Monster('늑대', 2, {max : 5, count : 5}, {max : 0, count : 0}, 1, 0, 7, 2,'늑대 이빨')
 
 // 전투 개시
 let inbattle = false
